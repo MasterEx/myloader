851b951fd923d413af21645b6de8cd5f33de21fd
Ammar Qammaz <ammarkov@gmail.com>
2010-05-20 01:50:03 +0300

    Many changes! :D

---
+++ file.php
@@ -0,0 +1,66 @@
+<?php
+
+/* THE MESSAGES CAN BE IMPROVED BUT SHOULD NOT TAKE MORE THAN 10 lines each in the php file*/
+function refuse_withoutcompression()
+{
+  echo "<html><body> 
+          <h2>Please dont send uncompressed images :P</h2>
+          I refuse to waste good bandwidth to upload this file..
+         </body></html>"; 
+}
+
+function link_not_found()
+{
+  echo "<html><body> 
+          <h2>Could not find file / File Expired ?</h2>
+          Your link is invalid , or maybe the file expired..
+         </body></html>"; 
+}
+
+$path = $_SERVER['DOCUMENT_ROOT']."/uploads/uploads/"; // change the path to fit your websites document structure
+$fullPath = $path.$_GET['i'];
+ 
+if ($fd = fopen ($fullPath, "r")) 
+{
+    $fsize = filesize($fullPath);
+    $path_parts = pathinfo($fullPath);
+    $ext = strtolower($path_parts["extension"]);
+    switch ($ext) 
+    {
+        case "pdf":
+        header("Content-type: application/pdf"); // add here more headers for diff. extensions
+        header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); // use 'attachment' to force a download
+        break;
+        case "png":
+        header("Content-type: image/png"); // add here more headers for diff. extensions    
+        break;     
+        case "gif":
+        header("Content-type: image/gif"); // add here more headers for diff. extensions         
+        break;
+        case "jpg":
+        header("Content-type: image/jpg"); // add here more headers for diff. extensions         
+        break;
+        case "jpeg":
+        header("Content-type: image/jpg"); // add here more headers for diff. extensions         
+        break;
+        case "bmp":
+         refuse_withoutcompression();
+        exit;      
+        break;
+        default;
+        header("Content-type: application/octet-stream"); 
+    }
+    header("Content-Disposition: filename=\"".$path_parts["basename"]."\"");
+    header("Content-length: $fsize");
+    header("Cache-control: private"); //use this to open files directly
+    while(!feof($fd)) {
+        $buffer = fread($fd, 2048);
+        echo $buffer;
+    }
+ fclose ($fd);
+} else
+{ 
+   link_not_found();
+}
+exit; 
+?>
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+   // VERSION 0.81
+    
+   $HOST_NAME="AmmarServer"; // Host Name
+   $SCRIPT_WEB_BASE="http://ammarkov.ath.cx/uploads/";
+   $LOCAL_PHP_FILE_LIMIT=20  *1024*1024; // 20 MB
+   $time_enter=microtime(true);
+?>
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="el" xml:lang="el">
 <head>
 <META name="author" content="Periklis Ntanasis a.k.a. Master_ex">
@@ -6,59 +14,115 @@
 <meta name="description" content="" />
 <meta name="distribution" content="global" />
 <title>My Personal Uploader</title>
+
+<style type="text/css">
+
+#Footnote
+   {     
+    color:#000000;
+    font-size:9px;
+    font-style:italic; 
+   }
+
+#Banner
+   {
+     background-image:url('<?php  $num=rand(1,3); echo $SCRIPT_WEB_BASE."images/banner_".$num.".jpg";?>');
+     background-repeat:no-repeat;
+     width:600px;
+     height:148px;
+     color:#EEEEEE;
+     font-size:33px;
+     font-weight:900; 
+     padding-top:55px;
+   }
+
+#Logo
+   {
+     visibility:hidden;
+   }
+
+a:link {color:#FF0000;}      /* unvisited link */
+a:visited {color:#CC0000;}  /* visited link */
+a:hover {color:#FF00FF;}  /* mouse over link */
+a:active {color:#0000FF;}  /* selected link */
+</style>
+
 </head>
-<body>
-<?php
-if(isset($_POST['submit']))
-{
-	$tmpdir = md5($_FILES['uploadedfile']['name'].date('l jS \of F Y h:i:s A'));
-	if($_FILES['uploadedfile']['size']>20000001)
-	{
-		echo "Files > 20Mb are not permitted";
-	}
-	else
-	{
-		if(mkdir("./uploads/".$tmpdir, 0700))
-		{
-			$target_path = "./uploads/".$tmpdir."/";
-			$target_path = $target_path.basename( $_FILES['uploadedfile']['name']);
-			$pieces = explode(".",$_FILES['uploadedfile']['name']);
-			if(strcmp($pieces[count($pieces)-1],"html")==0 || strcmp($pieces[count($pieces)-1],"htm")==0 )
-			{
-				$target_path = $target_path."s";
-			}
-			if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path))
-			{
-				copy("./uploads/msg.html","./uploads/".$tmpdir."/index.html");
-				echo "The file ".basename( $_FILES['uploadedfile']['name'])." has been uploaded<br/>";
-				echo "You can access the file <a href='$target_path'>here</a><br/>";
-			}
-			else
-			{
-				echo "There was an error uploading the file, please try again! 1 ";
-			}
-		}
-		else
-		{
-			echo "There was an error uploading the file, please try again! 2 ";
-		}
-	}
-	echo '<a href="index.php">Upload a new file!</a>';
-}
-else
-{
-?>
-
-<form enctype="multipart/form-data" action="index.php" method="POST">
-<input type="hidden" name="MAX_FILE_SIZE" value="33554432" />
-Choose a file to upload: <input name="uploadedfile" type="file" /><br />
-<input type="submit" value="Upload File" name="submit" />
-</form>
+<body><img id="Logo" src="images/logo.png">
+<div><center><br><br>
+<div id="Banner">MyUploads @ <?php echo $HOST_NAME; ?></div>
 
 <?php
+
+  if(isset($_POST['submit']))
+   {
+      $tmpdir = md5($_FILES['uploadedfile']['name'].date('l jS \of F Y h:i:s A'));
+      if($_FILES['uploadedfile']['size']>$LOCAL_PHP_FILE_LIMIT)
+       {
+         echo "Files > ".($LOCAL_PHP_FILE_LIMIT/1024*1024)." are not permitted";
+       }
+          else
+       {
+          if(mkdir("./uploads/".$tmpdir, 0700))
+           { 
+             $base_path = "./uploads/".$tmpdir."/";
+             
+             //New file.php file sender 
+             $new_target_path = "file.php?i=";
+             $new_target_path = $new_target_path.$tmpdir."/".basename( $_FILES['uploadedfile']['name']);
+
+             $target_path = $base_path.basename( $_FILES['uploadedfile']['name']);
+             $pieces = explode(".",$_FILES['uploadedfile']['name']);
+              
+             if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path))
+              {
+               copy("./uploads/msg.html","./uploads/".$tmpdir."/index.html");
+               echo "The file <b>".basename( $_FILES['uploadedfile']['name'])."</b> has been uploaded<br/>";
+               
+               echo "You can access the file <a href='$new_target_path' target=\"_new\">here</a><br/>";
+                
+              }
+                else
+              {
+                echo "There was an error uploading the file, please try again! 1 ";
+              }
+           }
+             else
+           {
+             echo "There was an error uploading the file, please try again! 2 ";
+           }
+       }
+     echo '<a href="index.php">Upload a new file!</a>';
+   }
+ else
+{  
+ echo
+  "
+   <table >
+   <tr height=10><td><td></tr>
+   <tr><td>
+   <form enctype=\"multipart/form-data\" action=\"index.php\" method=\"POST\">
+           <input type=\"hidden\" name=\"MAX_FILE_SIZE\" value=\"33554432\" />
+           File to upload: <input name=\"uploadedfile\" type=\"file\" /> 
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           <input type=\"submit\" value=\"Upload File\" name=\"submit\" /> 
+   </form>
+   </td></tr> 
+   </table>
+  ";
+ 
 }
 ?>
-<br/><br/>
-Every file should be < 20MB and the old files are deleted in daily basis.
+<br/><br/><br/><br/>
+<span id="Footnote">
+Every file should be < <?php $max_upload = (int)(ini_get('upload_max_filesize'));  echo $max_upload; ?> MB 
+and all the uploaded files are deleted in daily basis.<br>
+Written by <a href="http://periklis.is-a-geek.com/" target="_new">Master_Ex</a> , <a href="http://ammarkov.ath.cx/" target="_new">AmmarkoV</a><br><br>
+Generated in <?php echo (microtime(true)-$time_enter); ?> ms<br>
+</span>
+</center>
+</div>
 </body>
 </html>
+
+
