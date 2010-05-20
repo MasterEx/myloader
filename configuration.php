5204729da4927df63b68ce548294dee22969bcc2
Ammar Qammaz <ammarkov@gmail.com>
2010-05-20 11:09:55 +0300

    added check for easier testing of apache settings

--- configuration.php
+++ configuration.php
@@ -3,4 +3,5 @@
    $HOST_NAME="AmmarServer"; // Host Name
    $SCRIPT_WEB_BASE="http://ammarkov.ath.cx/uploads/";
    $LOCAL_PHP_FILE_LIMIT=20  *1024*1024; // 20 MB
+   $ENABLE_RANDOM_FILE = 1; // 1/0 Switch
 ?>
