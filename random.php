7d9e815296457b1cb9ac2ca5c5f05e6ae8251b67
Ammar Qammaz <ammarkov@gmail.com>
2010-05-20 11:15:49 +0300

    added random file possibility

---
+++ random.php
@@ -0,0 +1,12 @@
+<?php
+ require("configuration.php");
+ if ( $ENABLE_RANDOM_FILE  == 0 )
+  {
+    echo "<html><body>Random File Support is disabled</body></html>";
+  } else
+  {
+    /* TODO IMPLEMENT RANDOM FILE SEND */
+    echo "<html><body>Random File Support is not implemented :P </body></html>";
+  }
+
+?>
